bdd3d585b7dc993d5ca5f1f40e9cefb55a803257
Davide Brembilla <davide.brembilla98@gmail.com>
2024-12-10 11:23:26 +0100

    Adapt the PTP container to use apply the conversion for all labels
    This commit adapts the PTP container to the requirements in the PR, making the Ptp process execute to the entire volume

--- src/resources/assets/js/ptpContainer.vue
+++ src/resources/assets/js/ptpContainer.vue
@@ -4,16 +4,10 @@
         <div class="col-xs-12">
             <h3>Create a new Point to Polygon job (experimental)</h3>
             <span> Run the Point to polygon transformation using SAM</span><br>
-            <span>Select for which label should we run the Point to Point annotation:</span>
         </div>
       </div>
       <div class="row container-button-ptp">
-            <div class="col-xs-6">
-                <select class='form-control' v-model=selectedLabel >
-                    <option v-for="label in labels" :value="label.id">{{ label.name }}</option>
-                </select>
-            </div>
-            <div class="col-xs-6">
+           <div class="col-xs-6">
                 <a class="" target="_blank" title="Run Point to Polygon Conversion">
                     <i class="fa fa-draw-polygon big-button" aria-hidden="true" @click="sendPtpRequest()"></i>
                 </a>
@@ -30,30 +24,22 @@ export default {
     data(){
         let annotationsPerLabel = {};
         let imageIndexes = {};
-        biigle.$require('ptp.annotations').forEach(
-            function (ann) {
-                if (!annotationsPerLabel[ann['label_id']]){
-                    annotationsPerLabel[ann['label_id']]= [];
-                }
-                annotationsPerLabel[ann['label_id']].push(ann);
-            })
         return {
             imageIndexes: imageIndexes,
-            annotations: annotationsPerLabel,
-            showAnnotationOutlines: true,
-            labels: biigle.$require('ptp.labels'),
-            volumeId: biigle.$require('ptp.volumeId'),
+            volumeId: biigle.$require('volumes.volumeId'),
             selectedLabel: null,
         }
     },
+  props: {
+      volume: {
+          type: Number,
+          required: true,
+        }
+  },
    methods: {
 
         sendPtpRequest(){
-            if (!this.selectedLabel){
-                Messages.danger("No label selected!");
-                return
-            }
-            PtpJobApi.sendPtpJob({label_id: this.selectedLabel, volume_id: this.volumeId});
+            PtpJobApi.sendPtpJob({volume_id: this.volumeId});
         }
     }
 }
