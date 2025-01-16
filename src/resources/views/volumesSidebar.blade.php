a5132647e548d9d8cfa36608bdce2e6ee0213518
Davide Brembilla <davide.brembilla98@gmail.com>
2025-01-16 15:34:03 +0100

    Make PTP conversion sidebar visible only for valid volumes

--- src/resources/views/volumesSidebar.blade.php
+++ src/resources/views/volumesSidebar.blade.php
@@ -1,6 +1,8 @@
+@if (($user->can('edit-in', $volume) || $user->can('sudo')) && $volume->isImageVolume() && !$volume->hasTiledImages())
     <sidebar-tab name="ptp" icon="hat-wizard" title="Perform Point to Polygon conversion">
         <div id="ptp-container"></div>
     </sidebar-tab>
+@endif
 @push('scripts')
     <script src="{{ cachebust_asset('vendor/ptp/scripts/main.js') }}"></script>
 @endpush
