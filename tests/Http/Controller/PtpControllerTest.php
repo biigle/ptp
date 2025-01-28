0a6ca421d3a454bd0c9dfa7de52f7fec397ce94b
Davide Brembilla <davide.brembilla98@gmail.com>
2025-01-28 11:21:25 +0100

    Add new test for ptp_job_id

--- tests/Http/Controller/PtpControllerTest.php
+++ tests/Http/Controller/PtpControllerTest.php
@@ -5,6 +5,7 @@ namespace Biigle\Tests\Modules\Ptp\Controller;
 use ApiTestCase;
 use Biigle\MediaType;
 use Biigle\Role;
+use Biigle\Volume;
 use Biigle\Tests\ImageTest;
 use Biigle\Tests\ProjectTest;
 use Biigle\Tests\UserTest;
@@ -14,8 +15,9 @@ use Illuminate\Support\Facades\Queue;
 
 class PtpControllerTest extends ApiTestCase
 {
-    public function testCreateJob()
+    public function testCreateJob(): void
     {
+        //Test creating a Job with different types of users
         Queue::fake();
         $image = ImageTest::create(['volume_id' => $this->volume()->id]);
 
@@ -32,6 +34,32 @@ class PtpControllerTest extends ApiTestCase
         $this->beEditor();
         $this->postJson('/api/v1/send-ptp-job', ['volume_id' => $this->volume()->id])
             ->assertStatus(200);
+    }
+
+    public function testSetPtpJobId(): void
+    {
+        //Test that when creating a Job a ptp_job_id is set
+
+        Queue::fake();
+        $image = ImageTest::create(['volume_id' => $this->volume()->id]);
+
+        config(['ptp.ptp_storage_disk' => 'test']);
+        $this->doTestApiRoute('POST', '/api/v1/send-ptp-job');
+
+        $this->beEditor();
+        $this->postJson('/api/v1/send-ptp-job', ['volume_id' => $this->volume()->id])
+            ->assertStatus(200);
+
+
+        $volume = Volume::where('id', $this->volume()->id)->first();
+
+        $this->assertTrue(isset($volume->attrs['ptp_job_id']));
+
+        //If a ptp_job_id is set on a volume, we should get an error
+        $this->postJson('/api/v1/send-ptp-job', ['volume_id' => $this->volume()->id])
+            ->assertStatus(400);
+
+
     }
 
     public function testVideoVolumes()
